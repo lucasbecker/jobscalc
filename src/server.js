a4e965507d9e69232c9af64ec93013f040a5ecf3
Lucas Becker <lucasbeckerfelisberto@gmail.com>
2021-04-03 21:22:53 -0300

    second day

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const server = express();
 server.set('view engine', 'ejs');
 
 server.use(express.static('public'));
+server.use(express.urlencoded({ extended: true }));
 server.use(routes);
 
 server.listen(3000, () => console.log('app running...'));
